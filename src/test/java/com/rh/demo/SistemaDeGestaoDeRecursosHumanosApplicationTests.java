b48f6a9b8b75f79cf4a6448bee644ff0b2960063
DevMatheusSousa <dev.matheus.contato@gmail.com>
2025-12-29 10:20:31 -0300

    first commit

---
+++ src/test/java/com/rh/demo/SistemaDeGestaoDeRecursosHumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestaoDeRecursosHumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
