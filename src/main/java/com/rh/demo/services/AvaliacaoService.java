7d88750bc8eefa35fd1f526cd0ddee2612a7d46c
DevMatheusSousa <dev.matheus.contato@gmail.com>
2026-01-02 13:45:04 -0300

    feat: cria os service de AvaliacaoEntity

---
+++ src/main/java/com/rh/demo/services/AvaliacaoService.java
@@ -0,0 +1,89 @@
+package com.rh.demo.services;
+
+import com.rh.demo.mappers.AvaliacaoMapper;
+import com.rh.demo.model.DTOs.AvaliacaoDTO;
+import com.rh.demo.model.entites.AvaliacaoEntity;
+import com.rh.demo.repositories.AvaliacaoRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service // Indica que esta classe é um serviço do Spring
+public class AvaliacaoService {
+
+    private final AvaliacaoRepository avaliacaoRepository;
+    private final AvaliacaoMapper avaliacaoMapper;
+
+
+    public AvaliacaoService(AvaliacaoRepository avaliacaoRepository, AvaliacaoMapper avaliacaoMapper){
+        this.avaliacaoRepository = avaliacaoRepository;
+        this.avaliacaoMapper = avaliacaoMapper;
+    }
+
+    //listar todas as avaliações
+
+    /**
+     * Explicação detalhadado listarAvaliacoes:
+     * Este méto-do é responsável por listar todas as avaliações paginadas.
+     * Ele recebe um objeto Pageable como parâmetro, que contém informações sobre a página atual,
+     * o tamanho da página e a ordenação dos resultados.
+     * O méto-do utiliza o repositório de avaliações (avaliacaoRepository) para buscar todas as avaliações
+     * do banco de dados, aplicando a paginação conforme especificado pelo objeto Pageable.
+     * Em seguida, ele mapeia cada entidade AvaliacaoEntity para um DTO AvaliacaoDTO usando o avaliacaoMapper.
+     * O resultado é retornado como um objeto Page<AvaliacaoDTO>, que contém a lista paginada de avaliações no formato DTO.
+     * @param pageable Objeto Pageable que define a paginação e ordenação dos resultados.
+     * @return Página de AvaliacaoDTO contendo as avaliações paginadas.
+     *
+     * O que é a ‘interface’ Pageable e Paginacão?:
+     * A ‘interface’ Pageable é uma abstração fornecida pelo Spring Data que representa informações de paginação
+     * e ordenação para consultas de banco de dados. Ela permite que você especifique qual página de resultados
+     * deseja recuperar, quantos itens devem ser exibidos por página e como os resultados devem ser ordenados.
+     * A paginação é uma técnica usada para dividir grandes conjuntos de dados em partes menores e mais gerenciáveis,
+     * chamadas páginas. Isso é especialmente útil em aplicações ‘web’, onde exibir todos os resultados de uma vez pode ser ineficiente
+     * e sobrecarregar o utilizador com muita informação. Com a paginação, você pode carregar e exibir apenas um subconjunto dos dados
+     * por vez, melhorando a desempenho e a experiência do usuário.
+     */
+    public Page<AvaliacaoDTO> listarAvaliacoes(Pageable pageable){
+        return avaliacaoRepository
+                .findAll(pageable)
+                .map(avaliacaoMapper::toDTO);
+    }
+
+
+    //Listar todas as avaliações
+    public AvaliacaoDTO listarAvaliacaoPorId(Long id){
+        Optional<AvaliacaoEntity> avaliacaoEntity = avaliacaoRepository.findById(id);
+        if (avaliacaoEntity.isEmpty()) {
+            return null;
+        }
+        Optional<AvaliacaoDTO> avaliacaoPorId = avaliacaoEntity.map(avaliacaoMapper::toDTO); // Converte a entidade para DTO
+        return avaliacaoPorId.orElse(null);
+    }
+
+    //criar uma avaliação
+    public AvaliacaoDTO criarAvaliacao(AvaliacaoDTO avaliacaoDTO){
+        AvaliacaoEntity avaliacaoEntity = avaliacaoMapper.toEntity(avaliacaoDTO);
+        AvaliacaoEntity novaAvaliacao = avaliacaoRepository.save(avaliacaoEntity);
+        return avaliacaoMapper.toDTO(novaAvaliacao);
+    }
+
+    //deletar uma avaliação
+    public void deletarAvaliacao(Long id) {
+        avaliacaoRepository.deleteById(id);
+    }
+
+    //atualizar uma avaliação
+    public AvaliacaoDTO atualizarAvaliacao(Long id, AvaliacaoDTO avaliacaoDTO) {
+        Optional<AvaliacaoEntity> avaliacaoExistente = avaliacaoRepository.findById(id);
+        if (avaliacaoExistente.isPresent()) {
+            AvaliacaoEntity avaliacaoToUpdate = avaliacaoExistente.get();
+            avaliacaoToUpdate.setNotaAvaliacao(avaliacaoDTO.notaAvaliacao());
+            avaliacaoToUpdate.setFeedback(avaliacaoDTO.feedback());
+            AvaliacaoEntity avaliacaoAtualizada = avaliacaoRepository.save(avaliacaoToUpdate);
+            return avaliacaoMapper.toDTO(avaliacaoAtualizada);
+        } else {
+            return null;
+        }
+    }
+}
